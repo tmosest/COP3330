482e428329ef88c4a55089cee741230e33b098c7
Tyler Moses <tmosest@gmail.com>
2018-04-15 12:23:35 -0400

    Proj 7 almost done

---
+++ proj7/fstack.cpp
@@ -0,0 +1,163 @@
+/**
+    @file    fstack.cpp
+    @author  Tyler Moses
+    @date    04/13/2018
+    @version 1.0
+
+    Implementation of a stack test class.
+*/
+#include "tstack.h"
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <string>
+#include <cstdint>
+#include <cstring>
+
+// Default Width for columns
+const int COLUMN_WIDTH = 35;
+// Default Fill Character
+const char FILL = '.';
+fsu::Stack<int> stack;
+fsu::Stack<int> stack1(stack);
+
+/**
+ * @name DisplayWelcomeMessage
+ * Display a welcome message
+ */
+void DisplayWelcomeMessage()
+{
+  std::cout << "This is a Stack < int > test program" << std::endl;
+}
+
+/**
+ * @name PrintColumn
+ * Prints a menu item that is formatted.
+ * @param example Function expression.  EX: Pop()
+ * @param command User command:         EX: - or 2
+ */
+void PrintColumn(std::string example, std::string command)
+{
+  std::cout << std::left << std::setw(COLUMN_WIDTH) << std::setfill(FILL) << example << command << std::endl;
+}
+
+/**
+ * @name DisplayMenu
+ * Displays the menu options
+ */
+void DisplayMenu()
+{
+  PrintColumn("Push(Tval) ", " + tval or 1 tval");
+  PrintColumn("Pop() ", " - or 2");
+  PrintColumn("Clear() ", " C");
+  PrintColumn("Top() ", " T");
+  PrintColumn("Empty() ", " E");
+  PrintColumn("Size() ", " S");
+  PrintColumn("Copy Tests ", " =");
+  PrintColumn("Display() ", " D");
+  PrintColumn("SetOFC(ofc) ", " O 0|b|t|n");
+  PrintColumn("SetDIR(+1) ", " < (horizontal -->)");
+  PrintColumn("SetDIR(-1) ", " > (vertical   ^  )");
+  PrintColumn("Dump() ", " U");
+  PrintColumn("Display this menu ", " M");
+}
+
+void HandleSelection(char& selection)
+{
+  switch (selection)
+  {
+    // Push
+    case '1':
+    case '+':
+      int value;
+      std::cin >> value;
+      stack.Push(value);
+      break;
+      // Pop
+    case '2':
+    case '-':
+      stack.Pop();
+      break;
+      // Clear
+    case 'C':
+      stack.Clear();
+      break;
+    case 'T':
+      std::cout << "Top of Stack: " << stack.Top() << std::endl;
+      break;
+    case 'E':
+      std::cout << "Stack is ";
+      if (!stack.Empty())
+        std::cout << "not ";
+      std::cout << "empty" << std::endl;
+      break;
+    case 'S':
+      std::cout << std::left << std::setfill(' ') << std::setw(15) << "Stack size" << " = " << stack.Size() << std::endl;
+      break;
+    case '=':
+      /*std::cout << "CopyTest:\n";
+      stack1 = fsu::Stack<int>(stack);
+      std::cout << std::left << std::setw(20) << "Copied object\t" << "size: " << stack1.Size() << std::endl;
+      std::cout << std::left << std::setw(20) << "" << "contents: ";
+      stack1.Display(std::cout);
+      std::cout << std::endl;*/
+      break;
+    case 'D':
+      std::cout << "Stack Contents\t";
+      stack.Display(std::cout);
+      std::cout << std::endl;
+      break;
+    case 'O':
+      char ofc;
+      std::cin >> ofc;
+      switch (ofc)
+      {
+        case 'b':
+          ofc = ' ';
+          break;
+        case 't':
+          ofc = '\t';
+          break;
+        case 'n':
+          ofc = '\n';
+          break;
+        default:
+          ofc = '\0';
+      }
+      stack.SetOFC(ofc);
+      break;
+    case '<':
+      stack.SetDIR(1);
+      break;
+    case '>':
+      stack.SetDIR(-1);
+      break;
+    case 'U':
+      stack.Dump(std::cout);
+      break;
+    case 'M':
+      DisplayMenu();
+      break;
+    case 'Q':
+      std::cout << "\nHave a lovely day.\n";
+      break;
+    default:
+      std::cout << "\n** Unrecognized command -- please try again." << std::endl;
+  }
+}
+
+int main()
+{
+  stack.SetOFC(' ');
+  DisplayWelcomeMessage();
+  DisplayMenu();
+
+  char selection = 0;
+  while (selection != 'Q')
+  {
+    std::cout << "Enter [command][argument] ('M' for menu, 'Q' to quit): ";
+    std::cin >> selection;
+    selection = toupper(selection);
+    HandleSelection(selection);
+  }
+}
--- proj7/tstack.h
+++ proj7/tstack.h
@@ -11,59 +11,377 @@
 #define _TSTACK_H
 
 #include <iostream>
+#include <cstring>
+#include <iomanip>
 
 namespace fsu
 {
-  /**
-   * @name Stack
-   * Abstract DataType that represents a Stack.
-   * @tparam T
-   * @tparam N
-   */
-  template <typename T, size_t N = 100>
-  class Stack
-  {
+    /**
+     * @name Stack
+     * Abstract DataType that represents a Stack.
+     * @tparam T Type of Stack.
+     * @tparam N Capacity of the Stack.
+     */
+    template<typename T, size_t N = 100>
+    class Stack
+    {
     private:
-      // =========================
-      // Data
-      // =========================
-      const size_t capacity_;                // Maximum Capacity.
-      T * data_;                             // Dynamically allocated array.
-      size_t size_;                          // Current Size of the stack.
-      // =========================
-      // Display
-      // =========================
-      char ofc_;
-      int dir_;
+        // =========================
+        // Data
+        // =========================
+        const size_t capacity_;                 // Maximum Capacity.
+        T *data_;                               // Dynamically allocated array.
+        size_t size_;                           // Current Size of the stack.
+        // =========================
+        // Display
+        // =========================
+        char ofc_;
+        int dir_;
     public:
-      // =========================
-      // API
-      // =========================
-      void Push(const T& t);                 // Push t onto the stack.
-      T Pop();                               // Pop stack and remove the element; error if empty.
-      T& Top();                              // Return top element of the the stack; error if empty.
-      const T& Top() const;                  // const version
-      size_t Size() const;                   // return the number of elements in the stack.
-      size_t Capacity() const;               // return the max size of the stack.
-      bool Empty() const;                    // 1 if the stack is empty, 0 otherwise.
-      void Clear() const;                    // make the stack empty.
-      // =========================
-      // Constructor / Destructor
-      // =========================
-      Stack();                               // Default Constructor.
-      Stack(char ofc, int dir);              // Sets  ofc_ and dir_.
-      Stack(const Stack&);                   // Copy Constructor.
-      ~Stack();                              // Default Deconstructor.
-      Stack& operator = (const Stack&);      // Assignment Operator.
-      // =========================
-      // Display
-      // =========================
-      void Display (std::ostream& os) const; // Output stack contents.
-      void SetOFC(char ofc);                 // Set OFC
-      void SetDIR(int dir);                  // Set DIR
-      void Dump(std::ostream& os);           // Output all private data.
-  };
-  // end Stack template
+        // =========================
+        // API
+        // =========================
+        void Push(const T &t);                  // Push t onto the stack.
+        T Pop();                                // Pop stack and remove the element; error if empty.
+        T& Top();                               // Return top element of the the stack; error if empty.
+        const T& Top() const;                   // const version
+        size_t Size() const;                    // return the number of elements in the stack.
+        size_t Capacity() const;                // return the max size of the stack.
+        bool Empty() const;                     // 1 if the stack is empty, 0 otherwise.
+        void Clear();                           // make the stack empty.
+        // =========================
+        // Constructor / Destructor
+        // =========================
+        Stack();                                // Default Constructor.
+        Stack(char ofc, int dir);               // Sets  ofc_ and dir_.
+        Stack(const Stack &);                   // Copy Constructor.
+        ~Stack();                               // Default Deconstructor.
+        Stack &operator=(const Stack &);        // Assignment Operator.
+        // =========================
+        // Display
+        // =========================
+        void Display(std::ostream &os) const;   // Output stack contents.
+        void SetOFC(char ofc);                  // Set OFC
+        void SetDIR(int dir);                   // Set DIR
+        void Dump(std::ostream &os);            // Output all private data.
+    };
+    // end Stack template
+
+    // =========================
+    // API
+    // =========================
+
+    /**
+     * @name Push
+     * Add an element to the top of the stack.
+     * @tparam T
+     * @tparam N
+     * @param t
+     */
+    template<typename T, size_t N>
+    void Stack<T, N>::Push(const T &t)
+    {
+        if (size_ + 1 < capacity_)
+        {
+            data_[size_++] = t;
+            return;
+        }
+        // TODO else
+    }
+
+    /**
+     * @name Pop
+     * Returns the top element of the stack.
+     * @tparam T
+     * @tparam N
+     * @return
+     */
+    template<typename T, size_t N>
+    T Stack<T, N>::Pop()
+    {
+        if (!this->Empty())
+            return data_[size_--];
+        std::cerr << "** Stack error: Pop() called on Empty Stack\n";
+        return data_[0];
+    }
+
+    /**
+     * @name Top
+     * Look at the top element in the stack without popping it.
+     * Logs an error for overflow!
+     * @tparam T Type of the Stack
+     * @tparam N Capacity of the Stack
+     * @return Top element.
+     */
+    template<typename T, size_t N>
+    T& Stack<T, N>::Top()
+    {
+        if (!this->Empty())
+            return data_[size_];
+        std::cerr << "** Stack error: Pop() called on Empty Stack\n";
+        return data_[0];
+    }
+
+    /**
+     * @name Top
+     * Look at the top element in the stack without popping it.
+     * Logs an error for overflow!
+     * Constant Version
+     * @tparam T Type of the Stack
+     * @tparam N Capacity of the Stack
+     * @return Top element
+     */
+    template<typename T, size_t N>
+    const T& Stack<T, N>::Top() const
+    {
+        if (!this->Empty())
+            return data_[size_];
+        std::cerr << "** Stack error: Pop() called on Empty Stack\n";
+        return data_[0];
+    }
+
+    /**
+     * @name Size
+     * Gets the current size of the Stack.
+     * @tparam T Type of the Stack
+     * @tparam N Capacity of the Stack
+     * @return Size of the Stack
+     */
+    template<typename T, size_t N>
+    size_t Stack<T, N>::Size() const
+    {
+        return size_;
+    }
+
+    /**
+     * @name Capacity
+     * Gets the maximum size of the Stack.
+     * @tparam T Type of the Stack
+     * @tparam N Capacity of the Stack
+     * @return Capacity of the Stack
+     */
+    template<typename T, size_t N>
+    size_t Stack<T, N>::Capacity() const
+    {
+        return capacity_;
+    }
+
+    /**
+     * @name Empty
+     * Determines if the stack is empty or not.
+     * @tparam T Type of the Stack
+     * @tparam N Capacity of the Stack
+     * @return True if empty
+     */
+    template<typename T, size_t N>
+    bool Stack<T, N>::Empty() const
+    {
+        size_t empty = 0;
+        return size_ == empty;
+    }
+
+    /**
+     * @name Clear
+     * Clears the stack.
+     * @tparam T Type of the Stack
+     * @tparam N Capacity of the Stack
+     */
+    template<typename T, size_t N>
+    void Stack<T, N>::Clear()
+    {
+        delete data_;
+        data_ = new T[capacity_];
+        size_ = 0;
+    }
+
+    // =========================
+    // Constructor / Destructor
+    // =========================
+
+    /**
+     * @name Stack
+     * Default Constructor.
+     * @tparam T Type of the Stack
+     * @tparam N Capacity of the Stack
+     */
+    template<typename T, size_t N>
+    Stack<T, N>::Stack() : capacity_(N), size_(0), ofc_('\0'), dir_(1)
+    {
+      data_ = new T[capacity_];
+    }
+
+    /**
+     * @name Stack(char ofc, int dir)
+     * Constructor with output options.
+     * @tparam T Type of Stack
+     * @tparam N Capacity of the Stack
+     * @param ofc Seperator Character
+     * @param dir Direction of Stack for Printing
+     */
+    template <typename T, size_t N>
+    Stack<T, N>::Stack(char ofc, int dir)
+    {
+      Stack();
+      ofc_ = ofc;
+      dir_ = dir;
+    }
+
+    /**
+     * @name Stack(const Stack &stack)
+     * Copy constructor for the stack
+     * @tparam T Type of Stack
+     * @tparam N Capacity of the Stack
+     * @param stack Stack to copy
+     */
+    template <typename T, size_t N>
+    Stack<T, N>::Stack(const Stack &stack) : capacity_(stack.capacity_)
+    {
+        size_ = stack.size_;
+        data_ = new T[capacity_];
+        ofc_ = stack.ofc_;
+        dir_ = stack.dir_;
+        for (size_t i = 0; i < size_; i++)
+        {
+            data_[i] = stack.data_[i];
+        }
+    }
+
+    /**
+     * @name ~Stack
+     * Default Deconstructor
+     * @tparam T Type of Stack
+     * @tparam N Capacity of the Stack
+     */
+    template <typename T, size_t N>
+    Stack<T, N>::~Stack()
+    {
+        delete data_;
+    }
+
+    /**
+     * @name =
+     * Equals operator.
+     * @tparam T Type of Stack
+     * @tparam N Capacity of the Stack
+     * @param s Stack to be assigned to.
+     * @return Assigned stack
+     */
+    template <typename T, size_t N>
+    Stack<T,N>& Stack<T, N>::operator=(const Stack &s)
+    {
+        return s;
+    }
+
+    // =========================
+    // Display
+    // =========================
+
+    /**
+     * @name Display
+     * Displays the contents of the stack based on the dir and ofc.
+     * Positive dir indicate bottom-up vs top-down for negative.
+     * The ofc character is used to separate each item.
+     * @tparam T Type of Stack
+     * @tparam N Capacity of the Stack
+     * @param os Output Stream.
+     */
+    template<typename T, size_t N>
+    void Stack<T, N>::Display(std::ostream &os) const
+    {
+        // dir_determines the print ordering
+        if (dir_ < 0)
+        {
+            // Top to Bottom Ordering
+            for (int i = size_ -  1; i > -1; i--)
+            {
+                if (ofc_ != '\0')
+                    os << ofc_;
+                os << data_[i];
+            }
+        }
+        else
+        {
+            // Bottom Up Ordering
+            for (size_t i = 0; i < size_; i++)
+            {
+                if (ofc_ != '\0')
+                    os << ofc_;
+                os << data_[i];
+            }
+        }
+    }
+
+    /**
+     * @name SetOFC
+     * @tparam T Type of Stack
+     * @tparam N Capacity of the Stack
+     * @param ofc Char for Display separation
+     */
+    template<typename T, size_t N>
+    void Stack<T, N>::SetOFC(char ofc)
+    {
+        ofc_ = ofc;
+    }
+
+    /**
+     * @name SetDIR
+     * Sets the direction of the Stack for printing.
+     * @tparam T Type of Stack
+     * @tparam N Capacity of the Stack
+     * @param dir positive for bottom-up, negative for top-down
+     */
+    template<typename T, size_t N>
+    void Stack<T, N>::SetDIR(int dir)
+    {
+        dir_ = dir;
+    }
+
+    /**
+     * @name Dump
+     * Dumps the contents of the Stack.
+     * @tparam T Type of Stack
+     * @tparam N Capacity of the Stacks
+     * @param os output stream
+     */
+    template<typename T, size_t N>
+    void Stack<T, N>::Dump(std::ostream &os)
+    {
+        os << std::left << std::setfill(' ') << std::setw(15) << "Capacity:" << this->capacity_ << std::endl;
+        os << std::left << std::setfill(' ') << std::setw(15) << "Size:" << this->Size() << std::endl;
+        // TODO print array
+        os << std::left << std::setfill(' ') << std::setw(15) << "Array:" << "[";
+        for (std::size_t i = 0; i < capacity_; i++)
+        {
+            os << data_[i];
+            if (i != capacity_ - 1)
+                os << ",";
+        }
+        os << "]" <<  std::endl;
+        os << std::left << std::setfill(' ') << std::setw(15) << "Stack:" << "[";
+        for (std::size_t i = 0; i < size_; i++)
+        {
+            os << data_[i];
+            if (i != size_ - 1)
+                os << ",";
+        }
+        os << "]" << std::endl;
+    }
+
+    /**
+     * @name <<
+     * Override Stack output to Display
+     * @tparam T Type of Stack
+     * @tparam N Capacity of the Stack
+     * @param os output stream
+     * @param s Stacl
+     * @return output stream
+     */
+    template<typename T, size_t N>
+    std::ostream& operator << (std::ostream& os, const Stack<T,N>& s)
+    {
+        s.Display(os);
+        return os;
+    }
 }
 // end namespace fsu
 
